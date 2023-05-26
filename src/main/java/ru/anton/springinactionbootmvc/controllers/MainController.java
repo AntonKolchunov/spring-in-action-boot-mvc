8dddf91b3cc55af11a1c62765229d2e29f0cf5a9
Anton Kolchunov <kolcunovanton@gmail.com>
2023-05-26 16:43:58 +0300

    @RequestScope,@SessionScope,@ApplicationScope

--- src/main/java/ru/anton/springinactionbootmvc/controllers/MainController.java
+++ src/main/java/ru/anton/springinactionbootmvc/controllers/MainController.java
@@ -5,13 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.anton.springinactionbootmvc.services.LoggedUserManagementService;
+import ru.anton.springinactionbootmvc.services.LoginCountService;
 
 @Controller
 public class MainController {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService,
+                          LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
@@ -27,6 +31,7 @@ public class MainController {
         }
 
         model.addAttribute("username", username);
+        model.addAttribute("loginCount", loginCountService.getCount());
         return "main.html";
     }
 }
--- src/main/java/ru/anton/springinactionbootmvc/models/LoginProcessor.java
+++ src/main/java/ru/anton/springinactionbootmvc/models/LoginProcessor.java
@@ -3,6 +3,7 @@ package ru.anton.springinactionbootmvc.models;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 import ru.anton.springinactionbootmvc.services.LoggedUserManagementService;
+import ru.anton.springinactionbootmvc.services.LoginCountService;
 
 //Процесс авторизации делается через Spring Security, пока делаем вручную
 //@RequestScope ограничена областью видимости конкретного запроса
@@ -10,16 +11,21 @@ import ru.anton.springinactionbootmvc.services.LoggedUserManagementService;
 @RequestScope
 public class LoginProcessor {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
     private String userName;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService,
+                          LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     //Должны обращаться к БД, пока хардкодим
     //Возвращает результат аутентификации
     public boolean login() {
+        loginCountService.increment();
+
         String username = this.getUserName();
         String password = this.getPassword();
 
